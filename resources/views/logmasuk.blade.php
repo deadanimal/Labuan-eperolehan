768e9922ac77d7bbfce88c5c8bc1376ea4994148
Syafiza <syafiza.senin@pipeline-network.com>
2021-09-28 09:37:59 +0800

    28

--- resources/views/logmasuk.blade.php
+++ resources/views/logmasuk.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<link rel="icon" type="image/png" href="/Pl.png">
+	<link rel="icon" type="image/png" href="/PL.png">
 	<title>ePerolehan</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
@@ -13,7 +13,7 @@
 			<div class="row justify-content-sm-center h-100">
 				<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
 					<div class="text-center my-5">
-						<img src="/Pl.png" alt="logo" width="250">
+						<img src="/PL.png" alt="logo" width="250">
 					</div>
 					<div class="card shadow-lg">
 						<div class="card-body p-5">
